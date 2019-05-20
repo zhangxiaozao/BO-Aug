015666414217a1e51a63cd592b1f1d301bbdca60
zhangxiaozao <1761884934@qq.com>
2019-05-20 19:26:16 +0800

    Add files via upload

---
+++ robo/fmin/__init__.py
@@ -0,0 +1,29 @@
+try:
+    from .bayesian_optimization import bayesian_optimization
+except ImportError:
+    pass
+try:
+    from .random_search import random_search
+except ImportError:
+    pass
+try:
+    from .fabolas import fabolas
+except ImportError:
+    pass
+try:
+    from .mtbo import mtbo
+except ImportError:
+    pass
+try:
+    from .bohamiann import bohamiann
+except ImportError:
+    pass
+
+try:
+    from .entropy_search import entropy_search
+except ImportError:
+    pass
+try:
+    from .warmstart_mtbo import warmstart_mtbo
+except ImportError:
+    pass
